a5e0ee5eaca39cc715a00ced7ebe169e20d76b76
Ebrar <16155052@mersin.edu.tr>
2023-02-07 21:13:17 +0300

    first com

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
-import './globals.css'
+"use client"
+
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export default function RootLayout({
   children,
@@ -12,7 +17,13 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraProvider>
+          <Navbar/>
+        {children}
+        <Footer/>
+        </ChakraProvider>
+        </body>
     </html>
   )
 }
